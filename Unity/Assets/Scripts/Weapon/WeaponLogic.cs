127798766808dcd69ee40a4048b5371289b1471f
Alejandro <alejandroespinosa93@gmail.com>
2017-10-17 00:36:30 +0200

    Arreglar el bug que no mostraba el prefab

--- Unity/Assets/Scripts/Weapon/WeaponLogic.cs
+++ Unity/Assets/Scripts/Weapon/WeaponLogic.cs
@@ -32,7 +32,7 @@ namespace Assets.Scripts.Weapon
 
         public void Shoot()
         {
-            var weapon = CreateWeapon("Bomb", ShotStartingPoint);
+            var weapon = CreateWeapon("Armas/Bomba", ShotStartingPoint);
 
             SetWeaponVelocity(weapon, ShootingVelocity);
         }
