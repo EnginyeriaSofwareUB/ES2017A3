7428743ac3172a64c4c5db0e665d35b17fc94b4c
ainaheca <ainahernandezc@gmail.com>
2017-11-05 11:54:29 +0100

    se muestra el nombre del jugador que ha ganado

--- Unity/Assets/Scripts/EndGameScreenController.cs
+++ Unity/Assets/Scripts/EndGameScreenController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
@@ -8,15 +9,32 @@ public class EndGameScreenController : MonoBehaviour {
 
 	public Transform endGameCanvas; 
 	//private StateHolder stateHolder;
+	private GameManager gameManager;
+	public Text winnerTxt;
 
 
 	void Start() {
 		//this.stateHolder = GetComponent<StateHolder>();
+		this.gameManager = GetComponent<GameManager>();
+		fillWinnerText ();
 		endGameCanvas.gameObject.SetActive (false);
 	}
 
 
 
+	private void fillWinnerText(){
+		if (gameManager.isEmptyList (GameManager.LISTA_TOTEMS.LISTA_JUGADOR)) {
+			winnerTxt.text = "Player 2 wins!";
+		} else {
+			winnerTxt.text = "Player 1 wins!";
+		}
+	}
+
+
+
+
+
+	// BUTTON LISTENERS
 	public void PlayAgain(){
 		SceneManager.LoadScene ("JuegoScene");
 
