95c5f6736a265dde4b1820aa5765e1fd6b118134
ae11112 <aespinro11@alumnes.ub.edu>
2017-12-01 18:37:55 +0100

    island effects (V)

---
+++ Unity/Assets/PinwheelFantasyEffectFreePack2/Scripts/Fireball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+    public Rigidbody rgbd;
+    public GameObject fieryParticle;
+    public GameObject smokeParticle;
+    public GameObject explosionParticle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        fieryParticle.SetActive(false);
+        smokeParticle.SetActive(false);
+        explosionParticle.SetActive(true);
+        rgbd.constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
